7db5f5244fbf8887fd723fb5beccf3706910d46a
boulhouech <slim.boulahouech@gmail.com>
2024-04-16 15:14:34 +0100

    solving the problem algorithmically

---
+++ caesar_cipher.rb
@@ -0,0 +1,15 @@
+def caesar_cipher(text, shift)
+  encrypted_text = ""
+  text.each_char do |char|
+    if char.match?(/[a-zA-Z]/)
+      is_upper = char.match?(/[A-Z]/)
+      shifted = ((char.ord - (is_upper ? 'A'.ord : 'a'.ord) + shift) % 26 + (is_upper ? 'A'.ord : 'a'.ord)).chr
+      encrypted_text += shifted
+    else
+      encrypted_text += char
+    end
+  end
+  encrypted_text
+end
+
+
